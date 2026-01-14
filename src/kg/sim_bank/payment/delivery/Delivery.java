d5706a41846c6535b2d936899ab36919906860f6
aigerim312kg <adzhanybekova@yahoo.com>
2026-01-15 03:16:41 +0600

    Resolved conflict

---
+++ src/kg/sim_bank/payment/delivery/Delivery.java
@@ -0,0 +1,11 @@
+package kg.sim_bank.payment.delivery;
+
+public class Delivery {
+    static void main() {
+
+        System.out.println("Delivery Yandex");
+        System.out.println("Delivery Glovo");
+        System.out.println("Delivery Kover-Samolet");
+        System.out.println("Self Delivery");
+    }
+}
