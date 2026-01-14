e7694689a09cf0aa728367da708002380241a7ad
aigerim312kg <adzhanybekova@yahoo.com>
2026-01-14 23:31:10 +0600

    added sout and VisaPayment class

--- src/kg/sim_bank/payment/QrPayment.java
+++ src/kg/sim_bank/payment/QrPayment.java
@@ -1,8 +1,10 @@
 package kg.sim_bank.payment;
 
 public class QrPayment {
-    static void main() {
+    public static void main(String[] args) {
 
         System.out.println("QR Payment");
+        System.out.println("Hello World");
+
     }
 }
