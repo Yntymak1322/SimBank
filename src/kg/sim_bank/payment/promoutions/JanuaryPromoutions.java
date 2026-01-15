f9282ddc370668faffb72e2712efb13ae9d2d69f
aigerim312kg <adzhanybekova@yahoo.com>
2026-01-16 00:02:51 +0600

    January promo Yntymak.
    January promotion1
    
    January promotion2
    
    January promotions3
    
    January promotions4
    
    January promotion5
    
    January promotion6
    
    January promotion7

---
+++ src/kg/sim_bank/payment/promoutions/JanuaryPromoutions.java
@@ -0,0 +1,16 @@
+package kg.sim_bank.payment.promoutions;
+
+public class JanuaryPromoutions {
+    static void main() {
+
+        System.out.println("January ");
+        System.out.println("January123 ");
+        System.out.println("January1234 ");
+        System.out.println("January12345 ");
+        System.out.println("January12346 ");
+        System.out.println("January1234567 ");
+        System.out.println("January12345678 ");
+        System.out.println("January123456789 ");
+        System.out.println("January12345678910 ");
+    }
+}
