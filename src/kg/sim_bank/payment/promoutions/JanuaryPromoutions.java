f10c24008f10aeed9cd4a99625e24b65ae1e6ccb
aigerim312kg <adzhanybekova@yahoo.com>
2026-01-16 00:55:19 +0600

    4
    5
    
    6

--- src/kg/sim_bank/payment/promoutions/JanuaryPromoutions.java
+++ src/kg/sim_bank/payment/promoutions/JanuaryPromoutions.java
@@ -16,6 +16,9 @@ public class JanuaryPromoutions {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
 
     }
 }
